cde7cc49eb73c0e5e6f038c57a716413d454c9b4
amrit22 <amritrai22oct@gmail.com>
2025-03-25 20:58:30 +0530

    change from mongo to neon

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,15 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+
+// Serve frontend (React) build files
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+});
+
+
 // ✅ Middleware
 app.use(cors());
 app.use(express.json());
